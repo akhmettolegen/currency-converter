07ccb3e23bc1b34f78b45c7040bcc830e0a23ea2
Akhmet Tolegen <akhmet@sprintsquads.com>
2021-04-19 01:01:46 +0600

    feat: add currency rate instance in manager

--- pkg/converter/manager.go
+++ pkg/converter/manager.go
@@ -1,15 +1,38 @@
 package converter
 
-import "github.com/akhmettolegen/currency-converter/pkg/config"
+import (
+	"encoding/json"
+	"github.com/akhmettolegen/currency-converter/pkg/config"
+	"github.com/akhmettolegen/currency-converter/pkg/model"
+	"io/ioutil"
+	"os"
+)
 
 type Manager struct {
 	Config *config.Config
+	CurrencyRate *map[model.Currency]float64
 }
 
 func Get() *Manager{
 	config := config.Get()
 
+	codePath := os.Getenv("GOPATH") + "/src/github.com/akhmettolegen/currency-converter/pkg/converter/currency-rate.json"
+	jsonFile, err := os.Open(codePath)
+	codes := map[model.Currency]float64{}
+	if err == nil {
+		defer jsonFile.Close()
+		byteValue, err := ioutil.ReadAll(jsonFile)
+		if err != nil {
+			return nil
+		}
+
+		if err = json.Unmarshal(byteValue, &codes); err != nil {
+			return nil
+		}
+	}
+
 	return &Manager{
 		Config: config,
+		CurrencyRate: &codes,
 	}
 }
